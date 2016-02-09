c20c45c071dbcc7f0007eb328959430d4e5d0246
mike1796 <mickael_brunel@hotmail.fr>
2016-02-09 14:24:26 +0100

    Ajout des deux algos

---
+++ Pow.java
@@ -0,0 +1,39 @@
+public class Pow {
+    
+    /**
+     * Complexité en o(n)
+     */
+    public static int pow(int a, int n) {
+        int returnVal = a;
+        if (n == 0) {
+            return 1;
+        }
+        for (int i = 0; i < n - 1; ++i) {
+            returnVal *= a;
+        }
+        return returnVal;
+    }
+    
+    /**
+     * Complexité en o(log(n))
+     */
+    public static int pow2 (int a, int n) {
+        int temp;
+        if (y == 0) {
+            return 1;
+        }
+
+        temp = pow2(a, n/2);
+        if (n % 2 == 0) {
+            return temp * temp;
+        }
+        return a * temp * temp;
+        
+    }
+
+    public static void main (String[] args) {
+        int test = 2;
+        test = pow(test, 1);
+        System.out.println(test);
+    }
+}
